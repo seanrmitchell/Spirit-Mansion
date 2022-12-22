144e9d7401162868038b51395eeb721183a6f70a
seanmitchell <seanrmitchell211@gmail.com>
2022-12-22 17:15:10 -0500

    Player Movement v0.2
    Basically made the camera not spasm based off player movement.

--- BlackWhite/Assets/Scripts/PlayerMove.cs
+++ BlackWhite/Assets/Scripts/PlayerMove.cs
@@ -8,22 +8,13 @@ public class PlayerMove : MonoBehaviour
     public InputAction playerControls;
 
     [SerializeField] //visible in editor
-    private float maxSpeed, speed, turnSmoothing;
-
-    float turnVelocity;
-
-    [SerializeField]
-    private Transform groundCheck, cam;
-
-    [SerializeField]
-    private LayerMask groundObjects;
+    private float speed;
 
     [SerializeField]
     private Rigidbody rb;
 
-    Vector2 movement;
-    Vector2 mousePos;
-    Vector3 worldPos;
+    Vector2 movement, mousePos;
+    Vector3 direction;
 
     private void OnEnable()
     {
@@ -44,8 +35,10 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        // Player Looks at Mouse
         mousePos = Mouse.current.position.ReadValue();
-        //worldPos = Camera.main.ScreenToWorldPoint(mousePos);
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
         Plane ground = new Plane(Vector3.up, Vector3.zero);
         float rayLength;
@@ -59,27 +52,21 @@ public class PlayerMove : MonoBehaviour
         }
 
         // Gets horizontal input
+
         movement = playerControls.ReadValue<Vector2>();
-        Vector3 direction = new Vector3(movement.x, 0f, movement.y).normalized;
+        direction = new Vector3(movement.x, 0f, movement.y).normalized;
 
-        // calculates movement on ground
-        if (direction.magnitude >= 0.1f)
-        {
-
-
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoothing);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            // Makes dude move
-
-            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
-
-        }
     }
 
     private void FixedUpdate()
     {
         
+        // calculates movement on ground
+        if (direction.magnitude >= 0.1f)
+        {
+            // Makes dude move
+            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+        }
     }
 
     private void Attack(int x)
